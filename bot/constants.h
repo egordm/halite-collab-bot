f1adb4551b122523878ff2bb76c87fe5d16ceda7
Egor Dmitriev <egordmitriev2@gmail.com>
2017-11-20 11:03:20 +0100

    Updated constants Added constants for collision avoidance

--- bot/constants.h
+++ bot/constants.h
@@ -9,27 +9,36 @@
 #define ALLOW_KAMIKAZE false
 
 namespace bot {
-    namespace constants {
+	namespace constants {
 
 		constexpr unsigned int MIN_DOCK_SPOTS_FOR_COLONIZATION = 1;
 		constexpr double SHIP_AVOID_PROXIMITY = 10;
 
-        constexpr double AVOID_PROXIMITY_OFFSET = 0.2;
+		constexpr double AVOID_PROXIMITY_OFFSET = 0.2;
 		constexpr double DO_DEFEND = true;
-	    constexpr double DEFEND_SEARCH_RADIUS_DIRECTIONAL = hlt::constants::MAX_SPEED * 6;
-		constexpr double DEFEND_RADIUS = hlt::constants::DOCK_RADIUS + hlt::constants::WEAPON_RADIUS + hlt::constants::SHIP_RADIUS*2; // TODO: wont work it there are enemy planets closeby
+		constexpr double DEFEND_SEARCH_RADIUS_DIRECTIONAL = hlt::constants::MAX_SPEED * 6;
+		constexpr double DEFEND_RADIUS = hlt::constants::DOCK_RADIUS + hlt::constants::WEAPON_RADIUS +
+		                                 hlt::constants::SHIP_RADIUS * 2; // TODO: wont work it there are enemy planets closeby
 		constexpr double DEFEND_SEARCH_RADIUS = DEFEND_RADIUS * 3; // Optimal large radius
 		constexpr double DEFEND_RATIO = 0.6; // Optimal 0.4 TODO: change overtime
-	    constexpr double DEFENDERS_PER_ATTACKER = 0.8; // Optimal 0.6
+		constexpr double DEFENDERS_PER_ATTACKER = 0.8; // Optimal 0.6
 
-	    constexpr double DO_DISCOVERY = false;
+		constexpr double DO_DISCOVERY = false;
 		constexpr double DISCOVERY_RATIO = 0;
 
-	    constexpr double DISCOVERY_SEARCH_RADIUS = 100;
+		constexpr double DISCOVERY_SEARCH_RADIUS = 100;
 		constexpr unsigned int ATTACK_PLANET_ASSIGNMENT_MULTIPLIER = 3;
 
-        constexpr unsigned int ATTACK_PLANET_CHECK_RADIUS = 4;
-    }
+		constexpr unsigned int ATTACK_PLANET_CHECK_RADIUS = 4;
+
+		// NAVIGATION
+		constexpr float TIME_HORIZON = 1;
+		constexpr float TIME_HORIZON_OBSTACLE = 1;
+		constexpr float NEIGHBOR_DIST = TIME_HORIZON * (hlt::constants::MAX_SPEED);
+		constexpr unsigned int MAX_NEIGHBOURS = 500;
+		constexpr float RADIUS_MARGIN = 0.1;
+		constexpr unsigned int PLANET_VERTICES = 30;
+	}
 }
 
 
