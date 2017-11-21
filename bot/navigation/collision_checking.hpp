9ebf22379352a53c4c7423a40ff66196f4a23d67
Egor Dmitriev <egordmitriev2@gmail.com>
2017-11-21 22:19:56 +0100

    Updated collision checking. Added better formulae

--- bot/navigation/collision_checking.hpp
+++ bot/navigation/collision_checking.hpp
@@ -7,6 +7,8 @@
 
 #include <entities.hpp>
 #include <memory>
+#include <math.hpp>
+#include <cmath>
 #include <algorithm>
 
 namespace bot {
@@ -21,6 +23,40 @@ namespace bot {
 				const auto dist_trajec = kv->pos.dist_line(a, b);
 				if (dist_trajec < hlt::constants::SHIP_RADIUS + kv->radius) result.emplace_back(kv.get(), kv->pos);
 			}
+		}
+
+		static double time_of_closest_approach(const hlt::Vector &p1, const hlt::Vector &p2, const hlt::Vector &v1, const hlt::Vector &v2,
+		                       const double r1, const double r2, bool &collision) {
+			auto pab = p1 - p2;
+			auto vab = v1 - v2;
+			double a = vab.dot_prod(vab);
+			double b = 2 * pab.dot_prod(vab);
+			double c = pab.dot_prod(pab) - pow((r1 + r2), 2);
+
+			// Quadratic discriminant
+			double discriminant = b * b - 4 * a * c;
+
+			double t;
+
+			if (discriminant < 0) {
+				t = -b / (2 * a);
+				collision = false;
+			} else {
+				double t0 = (-b + sqrt(discriminant)) / (2 * a);
+				double t1 = (-b - sqrt(discriminant)) / (2 * a);
+				t = std::min(t0, t1);
+
+				collision = t >= 0;
+			}
+
+			return (t < 0) ? 0 : t;
+		}
+
+		static hlt::Vector point_of_collision(const hlt::Vector &p1, const hlt::Vector &p2, const hlt::Vector &v1, const hlt::Vector &v2,
+		                                const double r1, const double r2, const double t) {
+			const auto c1 = p1 + v1 * t;
+			const auto c2 = p2 + v2 * t;
+			return (c1 - c2) * (r2 / (r1 + r2)) + c2;
 		}
 	}
 }
