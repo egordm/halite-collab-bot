8495a5ebd617d4cdc6652335753edb0d61dd9dab
Egor Dmitriev <egordmitriev2@gmail.com>
2017-11-22 09:39:22 +0100

    Cleaned fast navigation to make it reusable in move correction

--- bot/navigation/navigation_fast.hpp
+++ bot/navigation/navigation_fast.hpp
@@ -8,6 +8,7 @@
 #include <sstream>
 #include <algorithm>
 #include <math.hpp>
+#include <functional>
 #include "../../hlt/move.hpp"
 #include "../../hlt/map.hpp"
 #include "../utils.h"
@@ -17,76 +18,144 @@
 using namespace std::placeholders;
 
 namespace bot {
-	namespace navigation {
-		constexpr double BYPASS_MARGIN = 0.2;
-		constexpr double SHIP_COLLISION_CHECK_RADIUS = hlt::constants::MAX_SPEED + 2 * hlt::constants::SHIP_RADIUS;
+    namespace navigation {
+        struct MovePromise {
+            const hlt::EntityId ship_id;
+            const hlt::MoveType type;
+            const hlt::Vector target;
+            hlt::EntityId dock_to;
+            hlt::Vector velocity;
 
-		// TODO: hitmap for path planning
-		class FastPath {
-		protected:
-			Observer &observer;
-			std::vector<hlt::EntityIdentifier> &ignore_list;
-			double target_speed;
+            MovePromise(const hlt::EntityId ship_id, const hlt::MoveType type, const hlt::EntityId dock_to,
+                        const hlt::Vector &velocity, const hlt::Vector &target)
+                    : ship_id(ship_id), type(type), dock_to(dock_to), velocity(velocity), target(target) {}
 
-			virtual void get_obstacles(const hlt::Vector &a, const hlt::Vector &b, std::vector<std::pair<hlt::Entity *, hlt::Vector>> &obst) {
-				check_collisions(observer.get_planets(), a, b, ignore_list, obst);
-				auto ships = observer.get_ships();
-				utils::erase_if(ships, std::bind(sorting::filter_by_status, hlt::ShipDockingStatus::Undocked, _1));
-				check_collisions(ships, a, b, ignore_list, obst);
-			}
+            hlt::Move produce() const {
+                switch (type) {
+                    case hlt::MoveType::Noop:
+                        return hlt::Move::noop();
+                    case hlt::MoveType::Thrust: {
+                        const double angle = std::round(hlt::rad_to_deg(velocity.angle()));
+                        const auto speed = std::min(7, static_cast<const int>(std::round(velocity.length())));
+                        return hlt::Move::thrust(ship_id, speed, static_cast<const int>(angle));
+                    }
+                    case hlt::MoveType::Dock:
+                        return hlt::Move::dock(ship_id, dock_to);
+                    case hlt::MoveType::Undock:
+                        return hlt::Move::undock(ship_id);
+                }
+            }
 
-			virtual hlt::Vector calc_avoid_point(const hlt::Vector &a, const hlt::Vector &b, const std::pair<hlt::Entity *, hlt::Vector> &obstacle,
-			                             const bool first_tangent) {
-				if (a.in_radius(obstacle.second, obstacle.first->radius + hlt::constants::SHIP_RADIUS + BYPASS_MARGIN)) {
-					return a.closest_point(obstacle.second, obstacle.first->radius + hlt::constants::SHIP_RADIUS + BYPASS_MARGIN);
-				}
+            static MovePromise noop() {
+                return {0, hlt::MoveType::Noop, 0, hlt::Vector(), hlt::Vector()};
+            }
 
-				auto tangents = a.tangents(obstacle.second, obstacle.first->radius + hlt::constants::SHIP_RADIUS + BYPASS_MARGIN);
+            static MovePromise dock(const hlt::EntityId ship_id, const hlt::EntityId dock_to) {
+                return {ship_id, hlt::MoveType::Dock, dock_to, hlt::Vector(), hlt::Vector()};
+            }
 
-				// Angle correction since converting it to int goes kind of bad
-				return math::compensate_shitint(obstacle.second, a, first_tangent ? tangents.first : tangents.second);
-			}
+            static MovePromise undock(const hlt::EntityId ship_id) {
+                return {ship_id, hlt::MoveType::Undock, 0, hlt::Vector(), hlt::Vector()};
+            }
 
-			virtual hlt::nullable<hlt::Vector>
-			correct_route(const hlt::Vector &a, const hlt::Vector &b, bool correct_left, unsigned int max_corrections) {
-				if (max_corrections == 0) return {b, false};
+            static MovePromise
+            thrust(const hlt::EntityId ship_id, const hlt::Vector &velocity, const hlt::Vector &target) {
+                return {ship_id, hlt::MoveType::Thrust, 0, velocity, target};
+            }
+        };
 
-				std::vector<std::pair<hlt::Entity *, hlt::Vector>> obst;
-				get_obstacles(a, b, obst);
-				if (obst.empty()) return {b, true};
 
-				auto closest_obst = *std::min_element(obst.begin(), obst.end(),
-				                                          std::bind(sorting::sort_obst_by_distance, a, _1, _2));
+        constexpr double BYPASS_MARGIN = 0.2;
+        constexpr double SHIP_COLLISION_CHECK_RADIUS = hlt::constants::MAX_SPEED + 2 * hlt::constants::SHIP_RADIUS;
+        constexpr double SHIP_BYPASS_MARGIN = 0.3;
 
-				return correct_route(a, calc_avoid_point(a, b, closest_obst, correct_left), correct_left, max_corrections - 1);
-			}
+        // TODO: hitmap for path planning
+        class FastPath {
+        protected:
+            Observer &observer;
+            std::vector<hlt::EntityIdentifier> ignore_list{};
+            double target_speed;
 
-		public:
-			FastPath(Observer &observer, std::vector<hlt::EntityIdentifier> &ignore_list, double target_speed)
-					: observer(observer), ignore_list(ignore_list), target_speed(target_speed) {}
+            /**
+             * TODO: use a vector of aversions
+             * @param a
+             * @param b
+             * @param aversions
+             */
+            virtual hlt::Vector pick_shortest_route(const hlt::Vector &a, const hlt::Vector &b,
+                                                    const std::vector<hlt::Vector> &aversions) {
+                hlt::Vector ret = *std::min_element(aversions.begin(), aversions.end(),
+                                                    [&a, &b](const hlt::Vector &e1, const hlt::Vector &e2) {
+                                                        return !e1.isnan() &&
+                                                               a.dist(e1) + b.dist(e1) < a.dist(e2) + b.dist(e2);
+                                                    });
 
-			MovePromise navigate(const hlt::Ship *ship, const hlt::Vector &target) {
-				ignore_list.push_back(ship->identify());
-				auto correction_left = correct_route(ship->pos, target, true, 30);
-				auto correction_right = correct_route(ship->pos, target, false, 30);
+                return ret.isnan() ? b : ret;
+            }
 
-				hlt::Vector corrected_target = target;
+            virtual void get_obstacles(const hlt::Vector &a, const hlt::Vector &b,
+                                       std::vector<std::pair<hlt::Entity *, hlt::Vector>> &obst) {
+                check_collisions(observer.get_planets(), a, b, ignore_list, obst);
+                auto ships = observer.get_ships();
+                utils::erase_if(ships, std::bind(sorting::filter_by_status, hlt::ShipDockingStatus::Undocked, _1));
+                check_collisions(ships, a, b, ignore_list, obst);
+            }
 
-				if (correction_left.second && correction_right.second) {
-					const auto left_is_shorter = (ship->pos.dist(correction_left.first) + target.dist(correction_left.first) <
-					                              ship->pos.dist(correction_right.first) + target.dist(correction_right.first));
-					corrected_target =  left_is_shorter ? correction_left.first : correction_right.first;
-				} else {
-					if (correction_left.second) corrected_target = correction_left.first;
-					if (correction_right.second) corrected_target = correction_right.first;
-				}
+            virtual hlt::Vector calc_avoid_point(const hlt::Vector &a, const hlt::Vector &b,
+                                                 const std::pair<hlt::Entity *, hlt::Vector> &obstacle,
+                                                 const bool first_tangent) {
+                if (a.in_radius(obstacle.second,
+                                obstacle.first->radius + hlt::constants::SHIP_RADIUS + BYPASS_MARGIN)) {
+                    return a.closest_point(obstacle.second,
+                                           obstacle.first->radius + hlt::constants::SHIP_RADIUS + BYPASS_MARGIN);
+                }
 
-				auto speed = std::min(ship->pos.dist(target), target_speed);
-				auto vel = math::resize_line(ship->pos, corrected_target, speed) - ship->pos;
-				return MovePromise::thrust(ship->entity_id, vel, target);
-			}
-		};
-	}
+                auto tangents = a.tangents(obstacle.second,
+                                           obstacle.first->radius + hlt::constants::SHIP_RADIUS + BYPASS_MARGIN);
+
+                // Angle correction since converting it to int goes kind of bad
+                return math::compensate_shitint(obstacle.second, a, first_tangent ? tangents.first : tangents.second);
+            }
+
+            virtual hlt::Vector
+            correct_route(const hlt::Vector &a, const hlt::Vector &b, bool correct_left, unsigned int max_corrections) {
+                if (max_corrections == 0) return hlt::Vector::NAN_VEC;
+
+                std::vector<std::pair<hlt::Entity *, hlt::Vector>> obst;
+                get_obstacles(a, b, obst);
+                if (obst.empty()) return b;
+
+                auto closest_obst = *std::min_element(obst.begin(), obst.end(),
+                                                      std::bind(sorting::sort_obst_by_distance, a, _1, _2));
+
+                return correct_route(a, calc_avoid_point(a, b, closest_obst, correct_left), correct_left,
+                                     max_corrections - 1);
+            }
+
+        public:
+            FastPath(Observer &observer, std::vector<hlt::EntityIdentifier> &ignore_list, double target_speed)
+                    : observer(observer), ignore_list(ignore_list), target_speed(target_speed) {}
+
+            FastPath(Observer &observer, double target_speed)
+                    : observer(observer), ignore_list(ignore_list), target_speed(target_speed) {}
+
+            MovePromise navigate(const hlt::Ship *ship, const hlt::Vector &target) {
+                ignore_list.push_back(ship->identify());
+
+                auto aversions = {
+                        correct_route(ship->pos, target, true, 30),
+                        correct_route(ship->pos, target, false, 30)
+                };
+
+                hlt::Vector corrected_target = pick_shortest_route(ship->pos, target, aversions);
+
+
+                auto speed = std::min(ship->pos.dist(target), target_speed);
+                auto vel = math::resize_line(ship->pos, corrected_target, speed) - ship->pos;
+                return MovePromise::thrust(ship->entity_id, vel, target);
+            }
+        };
+    }
 }
 
 #endif //HALITE_ORION_NAVIGATION_HPP_H
