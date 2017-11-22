211fac7221c006976f0840c0b91b374a82b7fad8
Egor Dmitriev <egordmitriev2@gmail.com>
2017-11-22 09:37:46 +0100

    Cleaned navigator & Implemented move correction

--- bot/navigation/navigator.cpp
+++ bot/navigation/navigator.cpp
@@ -4,73 +4,66 @@
 
 #include "../../hlt/navigation.hpp"
 #include "navigator.h"
-#include "../constants.h"
-#include "navigation_fast.hpp"
 
 namespace bot {
-	namespace navigation {
-		MovePromise LegacyNavigator::promise_dock_planet(const hlt::Ship *ship, const hlt::Planet *planet) {
-			if (ship->can_dock(planet)) {
-				return MovePromise::dock(ship->entity_id, planet->entity_id);
-			}
+    namespace navigation {
+        MovePromise LegacyNavigator::promise_dock_planet(const hlt::Ship *ship, const hlt::Planet *planet) {
+            if (ship->can_dock(planet)) {
+                return MovePromise::dock(ship->entity_id, planet->entity_id);
+            }
 
-			auto ret = hlt::navigation::navigate_ship_to_dock(observer.getMap(), ship, planet, hlt::constants::MAX_SPEED);
-			return (ret.second) ? MovePromise::thrust(ship->entity_id, ret.first.get_velocity(), planet->pos) : MovePromise::noop();
-		}
+            auto ret = hlt::navigation::navigate_ship_to_dock(observer.getMap(), ship, planet,
+                                                              hlt::constants::MAX_SPEED);
+            return (ret.second) ? MovePromise::thrust(ship->entity_id, ret.first.get_velocity(), planet->pos)
+                                : MovePromise::noop();
+        }
 
-		MovePromise LegacyNavigator::promise_attack_ship(const hlt::Ship *ship, const hlt::Ship *target) {
-			auto target_pos = ship->pos.closest_point(target->pos, target->radius + hlt::constants::WEAPON_RADIUS - 2);
-			auto ret = hlt::navigation::navigate_ship_towards_target(observer.getMap(), ship, target_pos, hlt::constants::MAX_SPEED,
-			                                                         true, 100, 0.02);
+        MovePromise LegacyNavigator::promise_attack_ship(const hlt::Ship *ship, const hlt::Ship *target) {
+            auto target_pos = ship->pos.closest_point(target->pos, target->radius + hlt::constants::WEAPON_RADIUS - 2);
+            auto ret = hlt::navigation::navigate_ship_towards_target(observer.getMap(), ship, target_pos,
+                                                                     hlt::constants::MAX_SPEED,
+                                                                     true, 100, 0.02);
 
-			return ret.second ? MovePromise::thrust(ship->entity_id, ret.first.get_velocity(), target_pos) : MovePromise::noop();
-		}
+            return ret.second ? MovePromise::thrust(ship->entity_id, ret.first.get_velocity(), target_pos)
+                              : MovePromise::noop();
+        }
 
-		MovePromise FastNavigator::promise_dock_planet(const hlt::Ship *ship, const hlt::Planet *planet) {
-			if (ship->can_dock(planet)) {
-				return MovePromise::dock(ship->entity_id, planet->entity_id);
-			}
+        MovePromise FastNavigator::promise_dock_planet(const hlt::Ship *ship, const hlt::Planet *planet) {
+            if (ship->can_dock(planet)) {
+                return MovePromise::dock(ship->entity_id, planet->entity_id);
+            }
 
-			auto target_pos = ship->pos.closest_point(planet->pos, ship->radius + planet->radius+ hlt::constants::DOCK_RADIUS - 1);
-			std::vector<hlt::EntityIdentifier> ignore_list {planet->identify()};
+            auto target_pos = ship->pos.closest_point(planet->pos,
+                                                      ship->radius + planet->radius + hlt::constants::DOCK_RADIUS - 1);
+            std::vector<hlt::EntityIdentifier> ignore_list{planet->identify()};
 
-			return navigation::FastPath(observer, ignore_list, 0).navigate(ship, target_pos);
-		}
+            return navigation::FastPath(observer, ignore_list, 0).navigate(ship, target_pos);
+        }
 
-		MovePromise FastNavigator::promise_attack_ship(const hlt::Ship *ship, const hlt::Ship *target) {
-			auto target_pos = ship->pos.closest_point(target->pos, ship->radius + target->radius + hlt::constants::WEAPON_RADIUS - 1);
+        MovePromise FastNavigator::promise_attack_ship(const hlt::Ship *ship, const hlt::Ship *target) {
+            auto target_pos = ship->pos.closest_point(target->pos,
+                                                      ship->radius + target->radius + hlt::constants::WEAPON_RADIUS -
+                                                      1);
 
-			std::vector<hlt::EntityIdentifier> ignore_list {target->identify()};
-			return navigation::fast::FastPath(observer, ignore_list).navigate(ship, target_pos);
-		}
+            std::vector<hlt::EntityIdentifier> ignore_list{target->identify()};
+            return navigation::FastPath(observer, ignore_list, hlt::constants::MAX_SPEED).navigate(ship, target_pos);
+        }
 
-		std::vector<hlt::Move> FastNavigator::produce_moves() {
-			/*for (const auto &ship : observer.get_my_ships()) { //TODO: ship correction
-				ship->vel = hlt::Vector();
-			}
-*/
-			std::vector<hlt::Move> ret;
-			hlt::entity_map<MovePromise> move_map;
-			for(const auto &move_promise : move_promises) {
-				// TODO: threat noop as displaceable
-				if(move_promise.type == hlt::MoveType::Thrust) {
-					observer.get_ship(move_promise.ship_id)->vel = move_promise.velocity;
-					move_map.emplace(move_promise.ship_id, move_promise);
-				} else {
-					observer.get_ship(move_promise.ship_id)->docking_status = hlt::ShipDockingStatus::Docked;
-					ret.push_back(move_promise.produce());
-				}
-			}
+        std::vector<hlt::Move> FastNavigator::produce_moves() {
+            for (const auto &ship : observer.get_my_ships()) { //TODO: ship correction
+                ship->vel = hlt::Vector();
+            }
 
 
+            for (const auto &move_promise : move_promises)
+                observer.get_ship(move_promise.ship_id)->vel = move_promise.velocity;
+            corrector.correct_moves(move_promises);
 
-			for (const auto &kv : move_map) {
-				auto promise = kv.second;
-				ret.push_back(promise.produce());
-			}
+            std::vector<hlt::Move> ret;
+            for (const auto &move_promise : move_promises) ret.push_back(move_promise.produce());
 
-			move_promises.clear();
-			return ret;
-		}
-	}
+            move_promises.clear();
+            return ret;
+        }
+    }
 }
--- bot/navigation/navigator.h
+++ bot/navigation/navigator.h
@@ -8,50 +8,11 @@
 
 #include "../../hlt/move.hpp"
 #include "../observer.h"
+#include "navigation_thorough.hpp"
 #include <cmath>
 
 namespace bot {
 	namespace navigation {
-		struct MovePromise {
-			const hlt::EntityId ship_id;
-			const hlt::MoveType type;
-			const hlt::Vector target;
-			hlt::EntityId dock_to;
-			hlt::Vector velocity;
-
-			MovePromise(const hlt::EntityId ship_id, const hlt::MoveType type, const hlt::EntityId dock_to, const hlt::Vector &velocity, const hlt::Vector &target)
-					: ship_id(ship_id), type(type), dock_to(dock_to), velocity(velocity), target(target) {}
-
-			hlt::Move produce() const {
-				switch (type) {
-					case hlt::MoveType::Noop: return hlt::Move::noop();
-					case hlt::MoveType::Thrust: {
-						const double angle = std::round(hlt::rad_to_deg(velocity.angle()));
-						const auto speed = std::min(7, static_cast<const int>(std::round(velocity.length())));
-						return hlt::Move::thrust(ship_id, speed, static_cast<const int>(angle));
-					}
-					case hlt::MoveType::Dock: return hlt::Move::dock(ship_id, dock_to);
-					case hlt::MoveType::Undock: return hlt::Move::undock(ship_id);
-				}
-			}
-
-			static MovePromise noop() {
-				return {0, hlt::MoveType::Noop, 0, hlt::Vector(), hlt::Vector()};
-			}
-
-			static MovePromise dock(const hlt::EntityId ship_id, const hlt::EntityId dock_to) {
-				return {ship_id, hlt::MoveType::Dock, dock_to, hlt::Vector(), hlt::Vector()};
-			}
-
-			static MovePromise undock(const hlt::EntityId ship_id) {
-				return {ship_id, hlt::MoveType::Undock, 0, hlt::Vector(), hlt::Vector()};
-			}
-
-			static MovePromise thrust(const hlt::EntityId ship_id, const hlt::Vector &velocity, const hlt::Vector &target) {
-				return {ship_id, hlt::MoveType::Thrust, 0, velocity, target};
-			}
-		};
-
 		class Navigator {
 		protected:
 			Observer &observer;
@@ -100,12 +61,14 @@ namespace bot {
 
 		class FastNavigator : public Navigator {
 		protected:
+			NavigationCorrector corrector;
+
 			MovePromise promise_dock_planet(const hlt::Ship *ship, const hlt::Planet *planet) override;
 
 			MovePromise promise_attack_ship(const hlt::Ship *ship, const hlt::Ship *target) override;
 
 		public:
-			explicit FastNavigator(Observer &observer) : Navigator(observer) {}
+			explicit FastNavigator(Observer &observer) : Navigator(observer), corrector(NavigationCorrector(observer)) {}
 
 			std::vector<hlt::Move> produce_moves() override;
 		};
